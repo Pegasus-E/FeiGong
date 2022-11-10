6dfc116ad4a95a634bf0a434af344fb02817524f
Pegasus-E <lm1669645496@gmail.com>
2022-11-10 22:35:54 +0800

    SpringBoot Change

---
+++ src/main/java/com/pegasus/feigong/FeiGong.java
@@ -0,0 +1,11 @@
+package com.pegasus.feigong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeiGong {
+    public static void main(String[] args) {
+        SpringApplication.run(FeiGong.class,args);
+    }
+}
